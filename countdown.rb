200f5a7634e78e0641c1d0bfafa0bf133a61a31f
Eric Song <ericksong91@gmail.com>
2022-12-08 17:31:51 -0500

    Done with assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
 #write your code here
 
-def countdown
+def countdown(i)
+    while i > 0
+        puts "#{i} SECOND(S)!"
+        i -= 1
+    end
+
+    return "HAPPY NEW YEAR!"
 end
+
+countdown(10)
+
+def countdown_with_sleep(i)
+
+    while i > 0
+        sleep(1.0)
+        puts "#{i} SECOND(S)!"
+        i -= 1
+    end
+
+    return "HAPPY NEW YEAR!"
+
+end
+
+countdown_with_sleep(10)
